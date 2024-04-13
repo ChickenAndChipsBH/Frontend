06179811f5a876f58d5fb5014b7619adcedcf7d2
charlotte-sk <118553231+charlotte-sk@users.noreply.github.com>
2024-04-13 14:05:15 +0100

    Initial commit

---
+++ ChickenAndChips/ChickenAndChips/ChickenAndChipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenAndChipsApp.swift
+//  ChickenAndChips
+//
+//  Created by Charlotte Chrysostom on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChickenAndChipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
