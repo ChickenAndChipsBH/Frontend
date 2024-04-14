834708da9961d52ab39bebe6a17a8ca394a6611b
TomYMuirhead <125360857+TomYMuirhead@users.noreply.github.com>
2024-04-14 09:16:29 +0100

    Update VUStaff.swift

--- ChickenAndChips/ChickenAndChips/VUStaff.swift
+++ ChickenAndChips/ChickenAndChips/VUStaff.swift
@@ -11,11 +11,11 @@ struct VUStaff: View {
     var body: some View {
         LazyVStack {
             // Staff Upgrades
-            CUpgradeable(title: "Quiz Master", description: "Hire someone to run the pub quiz while you’re away. +£10/sec", price: 2500)
-            CUpgradeable(title: "Bouncer", description: "Hire security to keep out troublemakers.", price: 4500)
-            CUpgradeable(title: "Bar Staff", description: "Hire staff to serve more customers. +£20 max income", price: 4000)
-            CUpgradeable(title: "Chef", description: "Hire kitchen staff to cook more food. +£10 max income", price: 2000)
-            CUpgradeable(title: "Cleaner", description: "Hire cleaning staff to keep the pub looking good. -5% rating decay", price: 1000)
+            CUpgradeable(title: "Quiz Master", description: "Hire someone to run the pub quiz while you’re away. +£10/sec", price: 2500, idleIncome: 10)
+            CUpgradeable(title: "Bouncer", description: "Hire security to keep out troublemakers. -10% rating decay", price: 4500, ratingDecayDecrease: 10)
+            CUpgradeable(title: "Bar Staff", description: "Hire staff to serve more customers. +£20 max income", price: 4000, incomeIncrease: 20)
+            CUpgradeable(title: "Chef", description: "Hire kitchen staff to cook more food. +£10 max income", price: 2000, incomeIncrease: 10)
+            CUpgradeable(title: "Cleaner", description: "Hire cleaning staff to keep the pub looking good. -5% rating decay", price: 1000, ratingDecayDecrease:5)
         }
     }
 }
