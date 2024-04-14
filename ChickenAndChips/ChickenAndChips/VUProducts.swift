312278234ed9dc2807e095445dabdc25efe4b3ba
TomYMuirhead <125360857+TomYMuirhead@users.noreply.github.com>
2024-04-14 09:24:11 +0100

    Update VUProducts.swift

--- ChickenAndChips/ChickenAndChips/VUProducts.swift
+++ ChickenAndChips/ChickenAndChips/VUProducts.swift
@@ -11,11 +11,11 @@ struct VUProducts: View {
     var body: some View {
         LazyVStack {
             // Products Upgrades
-            CUpgradeable(title: "Chicken N’ Chips", description: "Serve the team’s favourite meal.", price: 800)
-            CUpgradeable(title: "Snake Bite", description: "Serve the classic SU Happy Hour drink.", price: 500)
-            CUpgradeable(title: "Cocktails", description: "Charge loads for cocktails.", price: 1200)
-            CUpgradeable(title: "Burgers", description: "Who doesn’t like a burger?", price: 2000)
-            CUpgradeable(title: "Wine", description: "Rake in the cash by selling “fancy” wine.", price: 4000)
+            CUpgradeable(title: "Chicken N’ Chips", description: "Serve the team’s favourite meal. +£1 income", price: 800, incomeIncrease: 1)
+            CUpgradeable(title: "Snake Bite", description: "Serve the classic SU Happy Hour drink. +£2 income", price: 500, incomeIncrease: 2)
+            CUpgradeable(title: "Cocktails", description: "Charge loads for cocktails. +£5 income", price: 1200, incomeIncrease: 5)
+            CUpgradeable(title: "Burgers", description: "Who doesn’t like a burger? +£8 income", price: 2000, incomeIncrease: 8)
+            CUpgradeable(title: "Wine", description: "Rake in the cash by selling “fancy” wine. +£10 income", price: 4000, incomeIncrease: 10)
         }
     }
 }
